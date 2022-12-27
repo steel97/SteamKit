e777f24d3171b6230b6e307003ee37d66781958e
Ivan Yv <steel-97@mail.ru>
2022-12-27 04:49:08 +0300

    Proxy fixes

--- SteamKit2/SteamKit2/Steam/SteamClient/Configuration/ISteamConfigurationBuilder.cs
+++ SteamKit2/SteamKit2/Steam/SteamClient/Configuration/ISteamConfigurationBuilder.cs
@@ -5,6 +5,7 @@
 
 
 using System;
+using System.Net;
 using System.Net.Http;
 using SteamKit2.Discovery;
 
@@ -21,14 +22,14 @@ namespace SteamKit2
         /// </summary>
         /// <param name="cellID">The Steam Cell ID to prioritize when connecting.</param>
         /// <returns>A builder with modified configuration.</returns>
-        ISteamConfigurationBuilder WithCellID(uint cellID);
+        ISteamConfigurationBuilder WithCellID( uint cellID );
 
         /// <summary>
         /// Configures this <see cref="SteamConfiguration" /> with a connection timeout.
         /// </summary>
         /// <param name="connectionTimeout">The connection timeout used when connecting to Steam serves.</param>
         /// <returns>A builder with modified configuration.</returns>
-        ISteamConfigurationBuilder WithConnectionTimeout(TimeSpan connectionTimeout);
+        ISteamConfigurationBuilder WithConnectionTimeout( TimeSpan connectionTimeout );
 
         /// <summary>
         /// Configures this <see cref="SteamConfiguration" /> with the default <see cref="EClientPersonaStateFlag"/>s to request from Steam.
@@ -36,42 +37,42 @@ namespace SteamKit2
         /// <param name="personaStateFlags">The default persona state flags used when requesting information for a new friend, or
         /// when calling <c>SteamFriends.RequestFriendInfo</c> without specifying flags.</param>
         /// <returns>A builder with modified configuration.</returns>
-        ISteamConfigurationBuilder WithDefaultPersonaStateFlags(EClientPersonaStateFlag personaStateFlags);
+        ISteamConfigurationBuilder WithDefaultPersonaStateFlags( EClientPersonaStateFlag personaStateFlags );
 
         /// <summary>
         /// Configures this <see cref="SteamConfiguration" /> to discover available servers.
         /// </summary>
         /// <param name="allowDirectoryFetch">Whether or not to use the Steam Directory to discover available servers.</param>
         /// <returns>A builder with modified configuration.</returns>
-        ISteamConfigurationBuilder WithDirectoryFetch(bool allowDirectoryFetch);
+        ISteamConfigurationBuilder WithDirectoryFetch( bool allowDirectoryFetch );
 
         /// <summary>
         /// Configures this <see cref="SteamConfiguration" /> with custom HTTP behaviour.
         /// </summary>
         /// <param name="factoryFunction">A function to create and configure a new HttpClient.</param>
         /// <returns>A builder with modified configuration.</returns>
-        ISteamConfigurationBuilder WithHttpClientFactory(HttpClientFactory factoryFunction);
+        ISteamConfigurationBuilder WithHttpClientFactory( HttpClientFactory factoryFunction );
 
         /// <summary>
         /// Configures this <see cref="SteamConfiguration" /> with custom machine information.
         /// </summary>
         /// <param name="machineInfoProvider">A custom machine information provider.</param>
         /// <returns>A builder with modified configuration.</returns>
-        ISteamConfigurationBuilder WithMachineInfoProvider(IMachineInfoProvider machineInfoProvider);
+        ISteamConfigurationBuilder WithMachineInfoProvider( IMachineInfoProvider machineInfoProvider );
 
         /// <summary>
         /// Configures how this <see cref="SteamConfiguration" /> will be used to connect to Steam.
         /// </summary>
         /// <param name="protocolTypes">The supported protocol types to use when attempting to connect to Steam.</param>
         /// <returns>A builder with modified configuration.</returns>
-        ISteamConfigurationBuilder WithProtocolTypes(ProtocolTypes protocolTypes);
+        ISteamConfigurationBuilder WithProtocolTypes( ProtocolTypes protocolTypes );
 
         /// <summary>
         /// Configures the server list provider for this <see cref="SteamConfiguration" />.
         /// </summary>
         /// <param name="provider">The server list provider to use..</param>
         /// <returns>A builder with modified configuration.</returns>
-        ISteamConfigurationBuilder WithServerListProvider(IServerListProvider provider);
+        ISteamConfigurationBuilder WithServerListProvider( IServerListProvider provider );
 
         /// <summary>
         /// Configures the Universe that this <see cref="SteamConfiguration" /> belongs to.
@@ -79,7 +80,7 @@ namespace SteamKit2
         /// <param name="universe">The Universe to connect to. This should always be <see cref="EUniverse.Public"/> unless
         /// you work at Valve and are using this internally. If this is you, hello there.</param>
         /// <returns>A builder with modified configuration.</returns>
-        ISteamConfigurationBuilder WithUniverse(EUniverse universe);
+        ISteamConfigurationBuilder WithUniverse( EUniverse universe );
 
         /// <summary>
         /// Configures the Steam Web API address for this <see cref="SteamConfiguration" />.
@@ -87,7 +88,7 @@ namespace SteamKit2
         /// <param name="baseAddress">The base address of the Steam Web API to connect to.
         /// Use of "partner.steam-api.com" requires a Partner API Key.</param>
         /// <returns>A builder with modified configuration.</returns>
-        ISteamConfigurationBuilder WithWebAPIBaseAddress(Uri baseAddress);
+        ISteamConfigurationBuilder WithWebAPIBaseAddress( Uri baseAddress );
 
         /// <summary>
         /// Configures this <see cref="SteamConfiguration" /> with a Web API key to attach to requests.
@@ -95,6 +96,8 @@ namespace SteamKit2
         /// <param name="webApiKey">An API key to be used for authorized requests.
         /// Keys can be obtained from https://steamcommunity.com/dev or the Steamworks Partner site.</param>
         /// <returns>A builder with modified configuration.</returns>
-        ISteamConfigurationBuilder WithWebAPIKey(string webApiKey);
+        ISteamConfigurationBuilder WithWebAPIKey( string webApiKey );
+
+        ISteamConfigurationBuilder WithWebsocketsProxy( IWebProxy? webProxy );
     }
 }
